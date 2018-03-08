e035029c2136358e7c2126bb1b8c14941ee251c2
Gloye <gloye.sun@dookay.com>
2018-03-08 17:40:38 +0800

    熟悉下Dva的模式

--- project/src/router.js
+++ project/src/router.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Router, Route, Switch } from 'dva/router';
 import IndexPage from './routes/IndexPage';
+import User from './pages/users/page'
 
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
       <Switch>
         <Route path="/" exact component={IndexPage} />
+        <Route path="/users" exact component={User} />
       </Switch>
     </Router>
   );
