bb46ad1bb2005733ffc16fd430d436957c06bcb2
Gloye <dbzbook@163.com>
2018-04-17 17:47:39 +0800

    commit

--- project/src/choice/pages/question.js
+++ project/src/choice/pages/question.js
@@ -3,15 +3,16 @@ import _ from "lodash";
 
 function QuestionItem(props) {
   const Choices = [];
-  props.children.forEach((item, idx) => {
+  props.children.forEach((item, i) => {
     Choices.push(
       <Choice
         handleInput={props.handleInput}
         handleSubmit={props.handleSubmit}
-        val={item.title}
+        value={item.title}
         pindex={props.index}
-        index={idx}
-        key={idx}
+        index={i}
+        key={i}
+        isFocus={props.pindex===props.position[0]&&props.index===props.position[1]}
       />
     );
   });
@@ -54,12 +55,13 @@ function QuestionAdd(props) {
 
 function Choice(props) {
   const { index, pindex } = props;
+  const inputTip = 0 ? <SmartChoices choices={props.smartChoices} /> : null;
   return (
     <div>
       <input
         type="text"
         className="optionInput"
-        placeholder={props.val}
+        placeholder={props.value}
         onChange={e => props.handleInput(e)}
       />
 
@@ -71,23 +73,36 @@ function Choice(props) {
       >
         下一题
       </button>
-      <input
-        type="text"
-        className="resultInput"
-        onChange={e => {
-          props.handleInput(e);
-        }}
-      />
-      <button
-        className="resultSubmit"
-        onClick={e => {
-          props.handleSubmit(e);
-        }}
-      >
-        提交
-      </button>
+      <div style={{ position: "relative" }}>
+        <input
+          type="text"
+          className="resultInput"
+          onChange={e => {
+            props.handleInput(e, { index, pindex });
+          }}
+        />
+        <button
+          className="resultSubmit"
+          onClick={e => {
+            props.handleSubmit(e);
+          }}
+        >
+          提交
+        </button>
+        {inputTip}
+      </div>
     </div>
   );
+}
+
+function SmartChoices(props) {
+  let { choices } = props;
+  if (!!Array.isArray(choices)) {
+    choices.map(item => <li>item.title</li>);
+  } else {
+    choices = "test";
+  }
+  return <ul>{choices}</ul>;
 }
 
 /* 然后开始建第一个选项 */
@@ -98,8 +113,9 @@ class Question extends Component {
       title: null, // 用于提交标题赋值
       option: null, // 用于提交选项赋值
       result: null, // 用于提交结果赋值
-      position: [0, 0],
-      add: false
+      position: [-1, -1],
+      add: false,
+      smartChoices: null // 显示智能提示
     };
   }
 
@@ -112,6 +128,8 @@ class Question extends Component {
   /* 输入 */
   handleInput(e, ...args) {
     const { className } = e.target;
+    const { pindex, index } = args ? args[0] : { pindex: -1, index: -1 };
+    const position = [pindex,index]
     switch (className) {
       case "questionInput":
         const title = e.target.value;
@@ -123,11 +141,26 @@ class Question extends Component {
         break;
       case "resultInput":
         const result = e.target.value;
-        this.setState({ result });
+        this.smartType(result);
+        this.setState({
+          result,
+          position
+        });
         break;
       default:
         return null;
     }
+  }
+
+  /* 智能提示 */
+  smartType(inputVal) {
+    let { choices } = this.props.currentItem;
+    if (!choices||!Array.isArray(choices)) return;
+    const filterMethod = item => item.title.includes(inputVal);
+    const smartChoices = choices.filter(filterMethod);
+    this.setState({
+      smartChoices
+    });
   }
 
   checkPosition(positionDist) {
@@ -138,7 +171,7 @@ class Question extends Component {
   /* 提交 */
   handleSubmit(e, ...args) {
     e.preventDefault();
-    let { children,choices } = this.props.currentItem;
+    let { children, choices } = this.props.currentItem;
     // question id & choice id
     if (!Array.isArray(children)) {
       children = [];
@@ -149,21 +182,18 @@ class Question extends Component {
     const questionId = children.length + 1;
     const choiceId = 100 + choices.length + 1;
     // props method
-    const { createQuestion, updateOption } = this.props;
-    const { pindex, index } = args?args:{pindex:-1,index:-1};
+    const { createQuestion, updateOption,currentItem } = this.props;
+    const { pindex, index } = args ? args : { pindex: -1, index: -1 };
     const { className } = e.target;
     switch (className) {
       case "titleSubmit":
         const { title } = this.state;
         createQuestion(title);
         this.setState({
-          add:false
-        })
+          add: false
+        });
         break;
       case "optionSubmit":
-        /**
-         * props: title target(question/choice)
-         */
         const { option } = this.state;
         const targetId = this.checkPosition([pindex, index])
           ? choiceId
@@ -177,8 +207,9 @@ class Question extends Component {
       case "resultSubmit":
         const { result } = this.state;
         const { createAnswer } = this.props;
-        const position = [pindex, index];
-        this.setState(position);
+        if(pindex!==-1&&index!==-1){
+          currentItem.children[pindex].children[index].focus = true
+        }
         createAnswer(result);
         break;
       default:
@@ -187,15 +218,14 @@ class Question extends Component {
   }
 
   componentDidMount = () => {
-    const {children} = this.props.currentItem
-    if(!children){
+    const { children } = this.props.currentItem;
+    if (!children) {
       this.setState({
-        add:true
-      })
+        add: true
+      });
     }
-  }
-  
-  
+  };
+
   render() {
     const { title, children } = this.props.currentItem;
     const Questions = [];
@@ -203,6 +233,7 @@ class Question extends Component {
     const handleFocus = this.handleFocus.bind(this);
     const handleInput = this.handleInput.bind(this);
     const handleSubmit = this.handleSubmit.bind(this);
+    const {smartChoices,position} = this.state;
     const events = {
       handleFocus,
       handleInput,
@@ -211,7 +242,14 @@ class Question extends Component {
     if (children) {
       children.forEach((item, index) => {
         Questions.push(
-          <QuestionItem {...events} {...item} index={index} key={item.id} />
+          <QuestionItem
+            {...events}
+            {...item}
+            smartChoices={smartChoices}
+            position = {position}
+            index={index}
+            key={item.id}
+          />
         );
       });
     }
