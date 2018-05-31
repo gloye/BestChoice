dba504fb82972e190954325c2f875d473107cef0
gloye <dbzbook@hotmail.com>
2018-05-31 23:38:46 +0800

    commit

--- project/TodoList.md
+++ project/TodoList.md
@@ -23,11 +23,19 @@ cond3(no)->c1->e
 
 ## Todo list
 
-* [ ] option update
+* [x] option update
   * [x] disabled next button when the elment isn't empty
-  * [ ] choose a option
+  * [x] choose a option
   * [x] submit a option
     * [x] check the option
   * [x] show the value in input element
 * [x] enable change input's value
 * [x] change the statu when clear the choice for each item
+
+## 项目的一些思考
+
+由于工作上的原因，好久没有继续进行这个项目了，想起之前也进入了一个瓶颈，之前都是想到哪里做到哪里，只有一个大致的轮廓，现在想想接下来的方向要怎么去做。接下来要实现的完善交互体验。优先去做设计部分的功能，再去做体验部分的功能，最后去搭建后台，这是三步走的宏观实现，当前进度初步预估 50%了吧。
+
+## TodoList(2018/06)
+
+这个月的 Todo ， 主要包括三大方面吧， 一是核心功能， 二是调试入口，三是交互。
--- project/src/choice/App.js
+++ project/src/choice/App.js
@@ -1,135 +1,133 @@
-import React, { PureComponent } from "react";
-import _ from "lodash";
-import localforage from "localforage";
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import React, { PureComponent } from 'react'
+import _ from 'lodash'
+import lf from 'localforage'
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
 
-import Topic from "./pages/topic";
-import Question from "./pages/question";
-import Choice from "./pages/choice";
+import Topic from './pages/topic'
+import Question from './pages/question'
+import Choice from './pages/choice'
 
 const Nav = props => (
   <div>
     <ul>
       <li>
-        <Link to="/topic" onClick={this.createApp}>
-          创建问卷
-        </Link>
+        <Link to="/topic"> 创建问卷 </Link>
       </li>
       <li>
         <Link to="/question"> 查看上次记录 </Link>
       </li>
     </ul>
   </div>
-);
+)
 
-const NotFound = () => <h1>404 Not Found</h1>;
+const NotFound = () => <h1>404 Not Found</h1>
 
 class App extends PureComponent {
   constructor(props) {
-    super(props);
-    this.createApp = this.createApp.bind(this);
+    super(props)
+    this.createApp = this.createApp.bind(this)
     this.state = {
       currentItem: null
-    };
+    }
   }
 
   componentDidMount = () => {
     // 读取本地存储
-    localforage
-      .getItem("currentItem")
+    lf
+      .getItem('currentItem')
       .then(data => {
         if (!!data) {
-          this.upadateData(data);
+          this.upadateData(data)
         } else {
-          this.createApp();
+          this.createApp()
         }
       })
-      .catch(err => console.log(err));
-  };
+      .catch(err => console.log(err))
+  }
 
   /* 新建一个项目 */
-  createApp() {
-    localforage.removeItem("currentItem");
+  createApp(e) {
+    lf.removeItem('currentItem')
     this.setState({
       currentItem: {}
-    });
+    })
   }
 
   /* 新建一个主题 */
   createTitle(title) {
-    const currentItem = _.cloneDeep(this.state.currentItem);
-    if (!_.isPlainObject(currentItem)) return;
-    currentItem.title = title;
-    this.setState({ currentItem });
-    localforage.setItem("currentItem", currentItem);
+    const currentItem = _.cloneDeep(this.state.currentItem)
+    if (!_.isPlainObject(currentItem)) return
+    currentItem.title = title
+    this.setState({ currentItem })
+    lf.setItem('currentItem', currentItem)
   }
 
   /* 新建一个问题 */
   createQuestion(question) {
-    const currentItem = _.cloneDeep(this.state.currentItem);
-    let { children } = currentItem;
+    const currentItem = _.cloneDeep(this.state.currentItem)
+    let { children } = currentItem
     if (!_.isArray(children)) {
-      children = [];
+      children = []
     }
-    const options = this.initOption();
+    const options = this.initOption()
     const child = {
       title: question,
       id: children.length + 1,
       children: options
-    };
-    children.push(child);
-    currentItem.children = children;
-    this.upadateData(currentItem);
+    }
+    children.push(child)
+    currentItem.children = children
+    this.upadateData(currentItem)
   }
 
   /* 初始化选项 */
   initOption() {
     const options = [
       {
-        title: "是"
+        title: '是'
       },
       {
-        title: "否"
+        title: '否'
       }
-    ];
-    return options;
+    ]
+    return options
   }
 
   /* 更新数据 */
   upadateData(data) {
-    this.setState({ currentItem: data });
-    localforage.setItem("currentItem", data);
+    this.setState({ currentItem: data })
+    lf.setItem('currentItem', data)
   }
 
   /* 更改默认选项 */
   updateOption(o) {
-    const { index, pindex, option, target } = o;
-    const currentItem = _.cloneDeep(this.state.currentItem);
-    if (option) currentItem.children[pindex].children[index].title = option;
-    currentItem.children[pindex].children[index].target = target;
-    this.upadateData(currentItem);
+    const { index, pindex, option, target } = o
+    const currentItem = _.cloneDeep(this.state.currentItem)
+    if (option) currentItem.children[pindex].children[index].title = option
+    currentItem.children[pindex].children[index].target = target
+    this.upadateData(currentItem)
   }
 
   /* 新增一个结果 */
   createAnswer(o) {
-    const { index, pindex, target, result } = o;
-    const currentItem = _.cloneDeep(this.state.currentItem);
-    let { choices } = currentItem;
-    choices = _.isArray(choices) && !_.isEmpty(choices) ? choices : [];
+    const { index, pindex, target, result } = o
+    const currentItem = _.cloneDeep(this.state.currentItem)
+    let { choices } = currentItem
+    choices = _.isArray(choices) && !_.isEmpty(choices) ? choices : []
     // 提交一个空值时的逻辑 或者 先检测 choices
-    if (result === null || result === "") {
+    if (result === null || result === '') {
       // 删除target  删除 dest 删除 focus 状态重置
-      delete currentItem.children[pindex].children[index].target;
-      delete currentItem.children[pindex].children[index].dest;
-      delete currentItem.children[pindex].children[index].focus;
+      delete currentItem.children[pindex].children[index].target
+      delete currentItem.children[pindex].children[index].dest
+      delete currentItem.children[pindex].children[index].focus
     } else {
-      let push = true;
+      let push = true
       // 在choices中进行排除
       for (let i = 0; i < choices.length; i++) {
         if (choices[i].title === result) {
-          currentItem.children[pindex].children[index].target = choices[i].id;
-          push = false;
-          break;
+          currentItem.children[pindex].children[index].target = choices[i].id
+          push = false
+          break
         }
       }
       // 添加一个新选项
@@ -137,26 +135,26 @@ class App extends PureComponent {
         const choice = {
           title: result,
           id: target
-        };
-        choices.push(choice);
-        currentItem.children[pindex].children[index].target = target;
+        }
+        choices.push(choice)
+        currentItem.children[pindex].children[index].target = target
       }
     }
-    currentItem.choices = choices;
-    alert("提交成功");
-    this.upadateData(currentItem);
+    currentItem.choices = choices
+    alert('提交成功')
+    this.upadateData(currentItem)
   }
 
   clearResults() {
-    const currentItem = _.cloneDeep(this.state.currentItem);
-    currentItem.choices = [];
-    this.upadateData(currentItem);
+    const currentItem = _.cloneDeep(this.state.currentItem)
+    currentItem.choices = []
+    this.upadateData(currentItem)
   }
 
   render() {
-    const { currentItem } = this.state;
+    const { currentItem } = this.state
     if (!currentItem) {
-      return <div>加载中</div>;
+      return <div>加载中</div>
     } else {
       return (
         <Router>
@@ -180,7 +178,7 @@ class App extends PureComponent {
                     createAnswer={a => this.createAnswer(a)}
                     updateOption={o => this.updateOption(o)}
                     clearResults={() => {
-                      this.clearResults();
+                      this.clearResults()
                     }}
                     {...props}
                   />
@@ -192,9 +190,9 @@ class App extends PureComponent {
             </Switch>
           </div>
         </Router>
-      );
+      )
     }
   }
 }
 
-export default App;
+export default App
